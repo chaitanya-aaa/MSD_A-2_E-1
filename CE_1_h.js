4785cf13c3dd4678a2fcd09d9f502247bcbe2dd8
chaitanya-aaa <162866911+chaitanya-aaa@users.noreply.github.com>
2025-01-19 17:06:56 +0530

    Add files via upload

---
+++ CE_1_h.js
@@ -0,0 +1,6 @@
+/*1h.*/
+let tShirtPrice = 10;
+let socksPrice = 8;
+let dinnerPlatesPrice = 20;
+let totalCost = tShirtPrice + socksPrice + dinnerPlatesPrice;
+console.log(`Total cost of your order: $` + totalCost);
---
+++ CE_1_i.js
@@ -0,0 +1,7 @@
+/*1i.*/
+let bankAccount = 100;
+let lunchExpense = 20;
+let dinnerExpense = 50;
+let earnings = 200;
+let finalAmount = bankAccount - lunchExpense - dinnerExpense + earnings;
+console.log(`Final amount in your bank account: $` + finalAmount);
